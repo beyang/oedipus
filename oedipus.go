fa7a16b7c0ff52cff0fddf12513209575f81c4b6
Beyang Liu <beyang.liu@gmail.com>
2013-08-12 20:10:55 -0700

    initial parser

---
+++ oedipus.go
@@ -0,0 +1,139 @@
+package oedipus
+
+import (
+	"fmt"
+	"io/ioutil"
+	// "log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+)
+
+var sphinxCmd = "sphinx-build" // This should be set to the path to the sphinx command
+
+type Doc struct {
+	Symbol     string
+	Class      string
+	SourceFile string
+	Start      int
+	End        int
+	Body       string
+}
+
+func GetDocs(docDir string) ([]Doc, []error) {
+	buildDir := filepath.Join(docDir, "_build", "oedipus_html")
+	cacheDir := filepath.Join(docDir, "_build", "doctrees")
+	err := buildDocs(docDir, buildDir, cacheDir)
+	if err != nil {
+		return nil, []error{err}
+	}
+
+	return extractDocs(buildDir)
+}
+
+func buildDocs(sourceDir, buildDir, cacheDir string) error {
+	cmd := exec.Command(sphinxCmd, "-b", "html", "-d", cacheDir, sourceDir, buildDir)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("Error: %v with output:%s\n", err, string(out))
+	}
+	return nil
+}
+
+func extractDocs(buildDir string) (docs []Doc, errs []error) {
+	filepath.Walk(buildDir, func(path string, info os.FileInfo, err error) error {
+		ext := filepath.Ext(path)
+		if !info.IsDir() && (ext == ".txt" || ext == ".rst") {
+			if b, err := ioutil.ReadFile(path); err == nil {
+				body := string(b)
+				parser := new(docParseState)
+				theseDocs, theseErrs := parser.extractDocsFromHtml(body, "", true)
+
+				docs = append(docs, theseDocs...)
+				errs = append(errs, theseErrs...)
+			} else {
+				errs = append(errs, err)
+			}
+		}
+		return nil
+	})
+	return docs, errs
+}
+
+type docParseState struct {
+	index int
+
+	nextClassStart []int
+	nextClassEnd   []int
+	nextId         []int
+
+	Stack []Doc
+}
+
+var classStartTag = regexp.MustCompile(`\<dl(?: class="([^"]*)")?\>`)
+var classEndTag = regexp.MustCompile(`\</dl\>`)
+var idTag = regexp.MustCompile(`\<dt id="([^"]*)"\>`)
+
+func (d *docParseState) extractDocsFromHtml(html string, sourceFile string, includeSource bool) (docs []Doc, errs []error) {
+	d.Stack = make([]Doc, 0)
+	d.index = 0
+	d.nextClassStart = classStartTag.FindStringSubmatchIndex(html[d.index:])
+	d.nextClassEnd = classEndTag.FindStringIndex(html[d.index:])
+	d.nextId = idTag.FindStringSubmatchIndex(html[d.index:])
+	for {
+		// log.Printf("STATE: %+v", d)
+		if len(d.nextClassStart) > 0 &&
+			(len(d.nextClassEnd) == 0 || d.nextClassStart[0] < d.nextClassEnd[0]) &&
+			(len(d.nextId) == 0 || d.nextClassStart[0] < d.nextId[0]) {
+
+			if len(d.nextClassStart) == 4 {
+				className := html[d.nextClassStart[2]:d.nextClassStart[3]]
+				d.Stack = append(d.Stack, Doc{Class: className, SourceFile: sourceFile, Start: d.nextClassStart[0]}) // TODO:SourceFile
+				// log.Printf("<dl>: %s", className)
+			}
+
+			d.index = d.nextClassStart[1]
+			d.nextClassStart = classStartTag.FindStringSubmatchIndex(html[d.index:])
+			for i := 0; i < len(d.nextClassStart); i += 1 {
+				d.nextClassStart[i] += d.index
+			}
+		} else if len(d.nextClassEnd) > 0 &&
+			(len(d.nextId) == 0 || d.nextClassEnd[0] < d.nextId[0]) {
+			// log.Printf("</dl>")
+
+			doc := d.Stack[len(d.Stack)-1]
+			if doc.Class == "function" || doc.Class == "method" || doc.Class == "attribute" || doc.Class == "class" {
+				doc.End = d.nextClassEnd[1]
+				doc.Body = html[doc.Start:doc.End]
+				docs = append(docs, doc)
+			}
+			d.Stack = d.Stack[:len(d.Stack)-1]
+			d.index = d.nextClassEnd[1]
+			d.nextClassEnd = classEndTag.FindStringIndex(html[d.index:])
+			for i := 0; i < len(d.nextClassEnd); i += 1 {
+				d.nextClassEnd[i] += d.index
+			}
+		} else if len(d.nextId) > 0 {
+			// log.Printf("<dt>")
+			if len(d.nextId) == 4 {
+				d.Stack[len(d.Stack)-1].Symbol = html[d.nextId[2]:d.nextId[3]]
+			}
+
+			d.index = d.nextId[1]
+			d.nextId = idTag.FindStringSubmatchIndex(html[d.index:])
+			for i := 0; i < len(d.nextId); i += 1 {
+				d.nextId[i] += d.index
+			}
+		} else {
+			break
+		}
+
+	}
+
+	if len(d.Stack) > 0 {
+		return nil, []error{fmt.Errorf("Non-empty stack at end of parsing")}
+	}
+
+	return docs, errs
+}
---
+++ oedipus_test.go
@@ -0,0 +1,106 @@
+package oedipus
+
+import (
+	"testing"
+)
+
+func TestExtractDocsFromHtml(t *testing.T) {
+	testcases := []struct {
+		html string
+		docs []Doc
+	}{
+		{
+			html: `<body>
+<dl class="class">
+<dt id="foo">
+
+<dl class="method">
+<dt id="foo.bar">
+</dl>
+
+<dl class="method">
+<dt id="foo.baz">
+</dl>
+
+<dl class="data">
+<dt>
+</dl>
+
+</dl>
+</body>
+`,
+			docs: []Doc{
+				{
+					Symbol:     "foo.bar",
+					Class:      "method",
+					SourceFile: "test.html",
+					Body: `<dl class="method">
+<dt id="foo.bar">
+</dl>`,
+				},
+				{
+					Symbol:     "foo.baz",
+					Class:      "method",
+					SourceFile: "test.html",
+					Body: `<dl class="method">
+<dt id="foo.baz">
+</dl>`,
+				},
+				{
+					Symbol:     "foo",
+					Class:      "class",
+					SourceFile: "test.html",
+					Body: `<dl class="class">
+<dt id="foo">
+
+<dl class="method">
+<dt id="foo.bar">
+</dl>
+
+<dl class="method">
+<dt id="foo.baz">
+</dl>
+
+<dl class="data">
+<dt>
+</dl>
+
+</dl>`,
+				},
+			},
+		},
+	}
+
+	for _, testcase := range testcases {
+		parser := &docParseState{}
+		docs, errs := parser.extractDocsFromHtml(testcase.html, "test.html", true)
+
+		if len(errs) != 0 {
+			t.Errorf("Unexpected errors: %v", errs)
+		} else {
+			assertDocEqual(t, testcase.docs, docs)
+		}
+	}
+}
+
+func assertDocEqual(t *testing.T, exp []Doc, act []Doc) {
+	if len(exp) != len(act) {
+		t.Errorf("Expected %d docs, but found %d: %+v", len(exp), len(act), act)
+		return
+	}
+
+	for i := 0; i < len(exp); i++ {
+		if exp[i].Symbol != act[i].Symbol {
+			t.Errorf("Expected doc Symbol %s, but got %s", exp[i].Symbol, act[i].Symbol)
+		}
+		if exp[i].Class != act[i].Class {
+			t.Errorf("Expected doc Class %s, but got %s", exp[i].Class, act[i].Class)
+		}
+		if exp[i].SourceFile != act[i].SourceFile {
+			t.Errorf("Expected doc SourceFile %s, but got %s", exp[i].SourceFile, act[i].SourceFile)
+		}
+		if exp[i].Body != act[i].Body {
+			t.Errorf("Expected doc Body\n%s\nbut got\n%s", exp[i].Body, act[i].Body)
+		}
+	}
+}
